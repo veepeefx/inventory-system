6e9b89f20ec3e30d568f26c058302f0535f22af6
Veepeefx <194716914+veepeefx@users.noreply.github.com>
2025-12-22 20:21:24 +0200

    search ui to inventory view

--- src/View/InventoryView.cpp
+++ src/View/InventoryView.cpp
@@ -1,7 +1,9 @@
 #include "InventoryView.h"
 
+#include <QLabel>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QComboBox>
 
 #include "ItemEditorDialog.h"
 
@@ -20,8 +22,9 @@ InventoryView::InventoryView(DataBase& db, QWidget *parent)
     table_->setModel(model_);
 
     mainLayout_ = new QVBoxLayout(this);
-    mainLayout_->addWidget(table_);
 
+    initSearchBar();
+    mainLayout_->addWidget(table_);
     initInventoryControls();
 
     // removing visual focus from table
@@ -30,6 +33,75 @@ InventoryView::InventoryView(DataBase& db, QWidget *parent)
 }
 
 InventoryView::~InventoryView() {}
+
+void InventoryView::initSearchBar()
+{
+    QGridLayout* searchLayout = new QGridLayout();
+    searchLayout->setAlignment(Qt::AlignLeft);
+
+    QLabel* searchLabel = new QLabel("Search:");
+    searchLabel->setStyleSheet("font-size: 12pt; font-weight: bold;");
+
+    QLabel* search1Label = new QLabel("Parameter 1:");
+    QLabel* search2Label = new QLabel("Parameter 2:");
+
+    // multiple search bars add possibility to narrow down search
+    QLineEdit* search1LineEdit = new QLineEdit();
+    QLineEdit* search2LineEdit = new QLineEdit();
+    search1LineEdit_->setFixedWidth(200);
+    search2LineEdit_->setFixedWidth(200);
+
+    QComboBox* search1Type = new QComboBox();
+    QComboBox* search2Type = new QComboBox();
+    search1Type_->setFixedWidth(200);
+    search2Type_->setFixedWidth(200);
+
+    for (auto it = searchModeStrings.begin(); it != searchModeStrings.end(); it++) {
+        // SearchMode enum -> int
+        int key = static_cast<int>(it.key());
+        QString text = it.value();
+
+        search1Type_->addItem(text, key);
+        search2Type_->addItem(text, key);
+    }
+
+    // ADD USER TO HAVE POSSIBILITY TO CUSTOMIZE DEFAULT SEARCH TYPE
+    search1Type_->setCurrentIndex(static_cast<int>(SearchMode::NAME));
+    search2Type_->setCurrentIndex(static_cast<int>(SearchMode::PRODUCT_NUMBER));
+
+    QPushButton* searchButton = new QPushButton("Search");
+
+    connect(searchButton, &QPushButton::clicked, this,
+        [search1LineEdit, search2LineEdit, search1Type, search2Type, this]() {
+        makeSearch(search1LineEdit, search2LineEdit, search1Type, search2Type);
+    });
+
+    int row = 0;
+    searchLayout->addWidget(searchLabel, row, 0, 1, 1);
+
+    row++;
+
+    searchLayout->addWidget(search1Label, row, 0, 1, 1);
+    searchLayout->addWidget(search1LineEdit, row, 1, 1, 1);
+    searchLayout->addWidget(search1Type, row, 2, 1, 1);
+
+    row++;
+
+    searchLayout->addWidget(search2Label, row, 0, 1, 1);
+    searchLayout->addWidget(search2LineEdit, row, 1, 1, 1);
+    searchLayout->addWidget(search2Type, row, 2, 1, 1);
+
+    row++;
+
+    searchLayout->addWidget(searchButton, row, 0, 1, 1);
+
+    row++;
+
+    searchLayout->addItem( new QSpacerItem(0, 10, QSizePolicy::Expanding, QSizePolicy::Minimum),
+                           row, 3, 1, 1);
+
+    mainLayout_->addLayout(searchLayout);
+}
 
 void InventoryView::initInventoryControls()
 {
@@ -65,6 +137,22 @@ int InventoryView::selectedRowIndex() const
     }
 
     return row;
+}
+
+void InventoryView::makeSearch(QLineEdit* search1LineEdit, QLineEdit* search2LineEdit,
+                               QComboBox* search1Type, QComboBox* search2Type)
+{
+    QString qStr1 = search1LineEdit->text();
+    QString qStr2 = search2LineEdit->text();
+
+    if (qStr1.isEmpty() && qStr2.isEmpty()) {
+        return;
+    }
+
+    SearchMode mode1 = static_cast<SearchMode>(search1Type->currentIndex());
+    SearchMode mode2 = static_cast<SearchMode>(search2Type->currentIndex());
+
+    // SEARCH
 }
 
 void InventoryView::updateInventoryView()
--- src/View/InventoryView.h
+++ src/View/InventoryView.h
@@ -1,10 +1,27 @@
 #ifndef INVENTORY_SYSTEM_INVENTORYVIEW_H
 #define INVENTORY_SYSTEM_INVENTORYVIEW_H
 
+#include <QComboBox>
 #include <QTableView>
 
 #include "ItemTableModel.h"
 #include "../database/DataBase.h"
+
+enum class SearchMode {
+    NAME,
+    PRODUCT_NUMBER,
+    SELF_LOCATION,
+    EAN,
+    ID
+};
+
+static const QMap<SearchMode, QString> searchModeStrings {
+        { SearchMode::NAME, "Name" },
+        { SearchMode::PRODUCT_NUMBER, "Product Number" },
+        { SearchMode::SELF_LOCATION, "Self Location"},
+        { SearchMode::EAN, "EAN"},
+        { SearchMode::ID, "Unique ID"}
+};
 
 class InventoryView : public QWidget {
     Q_OBJECT
@@ -21,8 +38,19 @@ private:
     ItemTableModel* model_;
     DataBase& db_;
 
+    // search widgets
+    QLineEdit* search1LineEdit_;
+    QLineEdit* search2LineEdit_;
+    QComboBox* search1Type_;
+    QComboBox* search2Type_;
+
+    void initSearchBar();
     void initInventoryControls();
     int selectedRowIndex() const;
+
+    void makeSearch(QLineEdit* search1LineEdit, QLineEdit* search2LineEdit,
+                QComboBox* search1Type, QComboBox* search2Type);
+    void sqlSearch(std::string search, SearchMode mode);
 
 signals:
     void returnMainMenu();
