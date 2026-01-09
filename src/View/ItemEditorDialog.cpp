27f6c2b41b06494d68a85b66c8f554daf8ea4d1f
Veepeefx <194716914+veepeefx@users.noreply.github.com>
2026-01-09 19:49:06 +0200

    compacting initEditor function

--- src/View/ItemEditorDialog.cpp
+++ src/View/ItemEditorDialog.cpp
@@ -26,91 +26,48 @@ ItemEditorDialog::~ItemEditorDialog() {}
 void ItemEditorDialog::initEditor()
 {
     QGridLayout *editorLayout = new QGridLayout();
-
     int row = 0;
 
-    QLabel *idLabel = new QLabel("ID:");
-    idLineEdit_ = new QLineEdit(this);
-    idLineEdit_->setDisabled(true); // id isn't changable
-    editorLayout->addWidget(idLabel, row, 0, 1, 1);
-    editorLayout->addWidget(idLineEdit_, row, 1, 1, 1);
+    auto addField = [this, editorLayout, &row]
+    (const QString& labelText, bool nextRow = true, int col = 0, int colSpan = 1) {
+        QLabel* label = new QLabel(labelText, this);
+        QLineEdit* lineEdit = new QLineEdit(this);
+        editorLayout->addWidget(label, row, col);
+        editorLayout->addWidget(lineEdit, row, col + 1, 1, colSpan);
+        if (nextRow) { row++; }
+        return lineEdit;
+    };
 
-    row++;
+    auto addSpacer = [this, editorLayout, &row] () {
+        QLabel* label = new QLabel("", this);
+        editorLayout->addWidget(label, row, 0);
+        row++;
+    };
 
-    QLabel *productNumberLabel = new QLabel("Product Number:", this);
-    productNumberLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(productNumberLabel, row, 0, 1, 1);
-    editorLayout->addWidget(productNumberLineEdit_, row, 1, 1, 1);
+    idLineEdit_ = addField("ID:");
+    idLineEdit_->setDisabled(true);     // id cant be changed
 
-    QLabel *eanLabel = new QLabel("Ean:", this);
-    eanLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(eanLabel, row, 2, 1, 1);
-    editorLayout->addWidget(eanLineEdit_, row, 3, 1, 1);
+    productNumberLineEdit_ = addField("Product Number:", false);
+    eanLineEdit_ = addField("Ean:", true, 2);
 
-    row++;
+    nameLineEdit_ = addField("Name:", true, 0, 3);
 
-    QLabel *nameLabel = new QLabel("Name:", this);
-    nameLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(nameLabel, row, 0, 1, 1);
-    editorLayout->addWidget(nameLineEdit_, row, 1, 1, 3);
+    addSpacer();
 
-    row++;
+    priceLineEdit_ = addField("Price:", false);
+    priceNoVatLineEdit_ = addField("Price (excl. VAT):", false, 2);
+    vatLineEdit_ = addField("VAT:", true, 4);
 
-    QLabel* spacerLabel = new QLabel("", this);
-    editorLayout->addWidget(spacerLabel, row, 1, 1, 1);
+    discountedPriceLineEdit_ = addField("Discounted Price:", false);
+    discountLineEdit_ = addField("Discount:", true, 2);
 
-    row++;
+    addSpacer();
 
-    QLabel* priceLabel = new QLabel("Price:", this);
-    priceLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(priceLabel, row, 0, 1, 1);
-    editorLayout->addWidget(priceLineEdit_, row, 1, 1, 1);
+    quantityLineEdit_ = addField("Quantity:");
 
-    QLabel* priceNoVatLabel = new QLabel("Price (excl. VAT):", this);
-    priceNoVatLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(priceNoVatLabel, row, 2, 1, 1);
-    editorLayout->addWidget(priceNoVatLineEdit_, row, 3, 1, 1);
+    selfLocationLineEdit_ = addField("Self location:");
 
-    QLabel* vatLabel = new QLabel("VAT:", this);
-    vatLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(vatLabel, row, 4, 1, 1);
-    editorLayout->addWidget(vatLineEdit_, row, 5, 1, 1);
-
-    row++;
-
-    QLabel* discountedPriceLabel = new QLabel("Discounted Price:", this);
-    discountedPriceLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(discountedPriceLabel, row, 0, 1, 1);
-    editorLayout->addWidget(discountedPriceLineEdit_, row, 1, 1, 1);
-
-    QLabel* discountLabel = new QLabel("Discount:", this);
-    discountLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(discountLabel, row, 2, 1, 1);
-    editorLayout->addWidget(discountLineEdit_, row, 3, 1, 1);
-
-    row++;
-
-    editorLayout->addWidget(spacerLabel, row, 0, 1, 1);
-
-    row++;
-
-    QLabel* quantityLabel = new QLabel("Quantity:", this);
-    quantityLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(quantityLabel, row, 0, 1, 1);
-    editorLayout->addWidget(quantityLineEdit_, row, 1, 1, 1);
-
-    row++;
-
-    QLabel* selfLocationLabel = new QLabel("Self Location:", this);
-    selfLocationLineEdit_ = new QLineEdit(this);
-    editorLayout->addWidget(selfLocationLabel, row, 0, 1, 1);
-    editorLayout->addWidget(selfLocationLineEdit_, row, 1, 1, 1);
-
-    row++;
-
-    editorLayout->addWidget(spacerLabel, row, 0, 1, 1);
-
-    row++;
+    addSpacer();
 
     QLabel* descriptionLabel = new QLabel("Description:", this);
     descriptionTextEdit_ = new QTextEdit(this);
@@ -119,21 +76,12 @@ void ItemEditorDialog::initEditor()
 
     row += 4;
 
-    QLabel* lastModifiedLabel = new QLabel("Last Modified:", this);
-    lastModifiedLineEdit_ = new QLineEdit(this);
-    lastModifiedLineEdit_->setDisabled(true);
-    editorLayout->addWidget(lastModifiedLabel, row, 0, 1, 1);
-    editorLayout->addWidget(lastModifiedLineEdit_, row, 1, 1, 1);
+    lastModifiedLineEdit_ = addField("Last Modified:", false);
+    lastModifiedLineEdit_->setDisabled(true);   // updates automatically
+    createdLineEdit_ = addField("Created At:", true, 2);
+    createdLineEdit_->setDisabled(true);        // updates automatically
 
-    QLabel* createdLabel = new QLabel("Created:", this);
-    createdLineEdit_ = new QLineEdit(this);
-    createdLineEdit_->setDisabled(true);
-    editorLayout->addWidget(createdLabel, row, 2, 1, 1);
-    editorLayout->addWidget(createdLineEdit_, row, 3, 1, 1);
-
-    row++;
-
-    editorLayout->addWidget(spacerLabel, row, 0, 1, 1);
+    addSpacer();
 
     mainLayout_->addLayout(editorLayout);
 }
