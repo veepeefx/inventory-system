a8ee9981a46f18f606fc10d8bdafc3569851e814
Veepeefx <194716914+veepeefx@users.noreply.github.com>
2026-01-17 15:59:27 +0200

    preffered currency used in itemtable also

--- src/ui/inventory/tablemodels/ItemTableModel.cpp
+++ src/ui/inventory/tablemodels/ItemTableModel.cpp
@@ -1,5 +1,6 @@
 #include "ItemTableModel.h"
 
+#include "../../../SettingsManager.h"
 #include "../../../database/DataBase.h"
 
 ItemTableModel::ItemTableModel(DataBase& db, QWidget *parent)
@@ -60,25 +61,42 @@ int ItemTableModel::columnCount(const QModelIndex&) const
 
 QVariant ItemTableModel::data(const QModelIndex& index, int role) const
 {
-    if (!index.isValid() || role != Qt::DisplayRole) {
+    if (!index.isValid()) {
         return {};
     }
 
     const Item& item = items_[index.row()];
 
-    const double price = item.priceNoVat * (1 + item.vat/100) * (1 - item.discount/100);
+    if (role == Qt::DisplayRole) {
+        const double price = item.priceNoVat * (1 + item.vat/100.0) * (1 - item.discount/100.0);
 
-    switch (index.column()) {
-        case 0: return item.productNumber.data();
-        case 1: return item.name.data();
-        case 2: return item.quantity;
-        case 3: return item.priceNoVat;
-        case 4: return item.vat;
-        case 5: return item.discount;
-        case 6: return price;
-        case 7: return item.selfLocation.data();
-        default: return "NaN";
+        QString currency = SettingsManager::getSettings().currency;
+
+        switch (index.column()) {
+            case 0: return item.productNumber.data();
+            case 1: return item.name.data();
+            case 2: return item.quantity;
+            case 3: return QString::number(item.priceNoVat) + " " + currency;
+            case 4: return QString::number(item.vat) + " %";
+            case 5: return QString::number(item.discount) + " %";
+            case 6: return QString::number(price) + " " + currency;
+            case 7: return item.selfLocation.data();
+            default: return "NaN";
+        }
     }
+    
+    if (role == Qt::TextAlignmentRole) {
+        switch (index.column()) {
+            case 2: case 4: case 5:
+                return {Qt::AlignCenter | Qt::AlignVCenter};
+            case 3: case 6:
+                return {Qt::AlignRight | Qt::AlignVCenter};
+            default:
+                return {Qt::AlignLeft | Qt::AlignVCenter};
+        }
+    }
+
+    return {};
 }
 
 QVariant ItemTableModel::headerData(int section, Qt::Orientation orientation, int role) const
@@ -91,10 +109,10 @@ QVariant ItemTableModel::headerData(int section, Qt::Orientation orientation, in
         case 0: return "Product Number";
         case 1: return "Name";
         case 2: return "Quantity";
-        case 3: return "Price w/o vat (€)";
-        case 4: return "Vat (%)";
-        case 5: return "Discount (%)";
-        case 6: return "Price (€)";
+        case 3: return "Price w/o VAT";
+        case 4: return "VAT";
+        case 5: return "Discount";
+        case 6: return "Price";
         case 7: return "Self Location";
         default: return "NaN";
     }
